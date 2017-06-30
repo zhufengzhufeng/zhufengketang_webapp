90f1c9853ffdcfec94be8dcc0c857782e822b69d
zhufengzhufeng <894918097@qq.com>
2017-06-30 11:56:19 +0800

    transition

--- src/components/HomeHeader/index.js
+++ src/components/HomeHeader/index.js
@@ -1,16 +1,46 @@
 import React,{Component} from 'react';
 import './index.less';
 import logoImg from '../../common/images/logo.png';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 export default class HomeHeader extends Component{
+    constructor(){
+        super();
+        this.state = {
+            isShow:false
+        }
+    }
+    changeShow(){
+        this.setState({
+            isShow:!this.state.isShow
+        })
+    }
     render(){
+        let {isShow} = this.state;
         return (
             <div className="home-header">
                 <div className="header-bar">
                     <img src={logoImg}/>
-                    <div>
-                        <i className="iconfont icon-uilist"></i>
+                    <div onClick={this.changeShow.bind(this)}>
+                        {
+                            isShow?<i className="iconfont icon-guanbi"></i>:
+                                <i className="iconfont icon-uilist"></i>
+                        }
                     </div>
                 </div>
+                <div className="header-menu">
+                    <ReactCSSTransitionGroup
+                        transitionName="example"
+                        transitionEnterTimeout={500}
+                        transitionLeaveTimeout={300}>
+                        {isShow?<ul>
+                            <li>Node课程培训</li>
+                            <li>HTML课程培训</li>
+                            <li>视频课程</li>
+                            <li>文档课程</li>
+                        </ul>:''}
+                    </ReactCSSTransitionGroup>
+                </div>
+
             </div>
         )
     }
